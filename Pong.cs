03ed63d372842c8c23f95509f8ede32dc0adfb13
Riley Kroetsch <rkroetsch7@gmail.com>
2026-02-10 13:23:38 -0700

    Reorganized and commented code

--- Pong.cs
+++ Pong.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -10,36 +11,77 @@ public class Pong : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
-    private Rectangle _BallRect;
+    
 
-    // Sprites
+    
 
-        // Background
-        private Texture2D _BackgroundCourt;
+    // Background Sprite
+    private Texture2D _BackgroundCourt;
 
-        // Paddles
-        private Texture2D _PaddleL;
-        private Texture2D _PaddleR;
+       
+       
 
-        // Ball
+    // Ball
+
+        // Ball Texture
         private Texture2D _Ball;
-
-    // Vectors
         
+        // Ball Hixbox Rectangle
+        private Rectangle _BallRect;
+
+        // Ball Width & Height
+        private const int _BallWidthHeight = 21;
+
+        // Ball Speed
+        private float _BallSpeed;
+
+        // Ball Position & Direction
+        private Vector2 _BallPosition, _BallDirection;
+
+
+    // Paddle
+
+        // Paddle Texture
+        private Texture2D _Paddle;
+
+        // Paddle Left and Right Vecotors
         private Vector2 _PaddleLeft;
         private Vector2 _PaddleRight; 
 
+        // Paddle Hitbox Rectangles
+        private Rectangle _PaddleRectL;
+        private Rectangle _PaddleRectR;
+
+        // Paddle Width & Height
+        private const int _PaddleWidth = 16, _PaddleHeight = 125;
+
+    
+    // Game Manager
+
+        // Sets Window Size Paramaters
+        private const int _WindowWidth = 750, _WindowHeight = 450;
+
+        // Screen Edge Collision Detection
+        private Rectangle _playAreaBoundingBox;
+
 
         
 
-    private const int _WindowWidth = 750, _WindowHeight = 450;
-    private const int _PlayAreaEdgeLineWidth = 12, _BallWidthHeight = 21;
+    
+    
 
-    private float _BallSpeed;
+    
 
-    private Vector2 _BallPosition, _BallDirection;
 
-    private Rectangle _playAreaBoundingBox;
+    
+
+    
+
+    
+
+  
+
+   
 
     
 
@@ -53,14 +95,17 @@ public class Pong : Game
 
     protected override void Initialize()
     {
+        // Sets Window Width and Height
         _graphics.PreferredBackBufferWidth = _WindowWidth;
         _graphics.PreferredBackBufferHeight = _WindowHeight;
         _graphics.ApplyChanges();
 
+        // Sets Ball Starting Position and speed
         _BallPosition.X = 350;
         _BallPosition.Y = 225;
         _BallSpeed = 100;
 
+        // Sets Ball Starting Direction
         _BallDirection.X = 1;
         _BallDirection.Y = 1;
 
@@ -69,12 +114,14 @@ public class Pong : Game
           // Sets Paddle Positions
 
             // X
-            _PaddleLeft.X = 50;
-            _PaddleRight.X = 700;
+            _PaddleLeft.X = 40;
+            _PaddleRight.X = 696;
 
             // Y
             _PaddleLeft.Y = 225;
             _PaddleRight.Y = 225;
+
+            
 
 
 
@@ -91,8 +138,8 @@ public class Pong : Game
             _BackgroundCourt = Content.Load<Texture2D>("Court");
 
         // Loads Paddle Left and Right
-            _PaddleL = Content.Load<Texture2D>("Paddle");
-            _PaddleR = Content.Load<Texture2D>("Paddle");
+          
+            _Paddle = Content.Load<Texture2D>("Paddle");
 
         // Loads Ball
             _Ball = Content.Load<Texture2D>("Ball");
@@ -108,26 +155,116 @@ public class Pong : Game
             Exit();
 
         // TODO: Add your update logic here
-         _BallPosition += _BallDirection * _BallSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
+        // Updates the balls position over time
+        _BallPosition += _BallDirection * _BallSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        // Creates ball Hitbox
         _BallRect = new Rectangle((int)_BallPosition.X, (int)_BallPosition.Y,_BallWidthHeight, _BallWidthHeight);
 
-        // Checks Collision for the walls
+        // Creates Paddles Left & Right Hitbox
+        _PaddleRectL = new Rectangle((int)_PaddleLeft.X, (int)_PaddleLeft.Y, _PaddleWidth, _PaddleHeight);
+        _PaddleRectR = new Rectangle((int)_PaddleRight.X, (int)_PaddleRight.Y, _PaddleWidth, _PaddleHeight);
 
-            // If Ball Touches the Left or Right
-            if(_BallRect.Left <= _playAreaBoundingBox.Left ||
-            _BallRect.Right >= _playAreaBoundingBox.Right)
-            {
-                _BallDirection.X*=-1;
-            }
-            // If Ball Touches the Top or Bottom
-            if(_BallRect.Bottom >= _playAreaBoundingBox.Bottom ||
-            _BallRect.Top <= _playAreaBoundingBox.Top)
-            {
-                _BallDirection.Y*=-1;
-            }
+        // Checks Player 1 (WASD) & 2 (Arrow Keys) Keyboard input
+        checkInputs();
+        // Checks if the Ball or Paddles are Touching the wall
+        wallCollisionCheck();
+        // Checks if Ball is Touching the Paddle
+        paddleCollisionCheck();
+
+   
+
+        
+
+        
 
         base.Update(gameTime);
+    }
+
+    private void checkInputs()
+    {
+        // Player 1 (WASD)
+        if (Keyboard.GetState().IsKeyDown(Keys.W))
+        {
+            // Left Paddle Up
+            _PaddleLeft.Y -= 5;
+        }
+         if (Keyboard.GetState().IsKeyDown(Keys.S))
+        {
+            // Left Paddle Down
+            _PaddleLeft.Y += 5;
+        }
+        // Player 2 (Arrow Keys)
+        if (Keyboard.GetState().IsKeyDown(Keys.Up))
+        {
+            // Right Paddle Up
+            _PaddleRight.Y -= 5;
+        }
+        if (Keyboard.GetState().IsKeyDown(Keys.Down))
+        {
+            // Right Paddle Down
+            _PaddleRight.Y += 5;
+        }
+       
+    }
+    private void wallCollisionCheck()
+    {
+    // Ball Bounds
+
+        // If Ball Touches the Left or Right
+        if(_BallRect.Left <= _playAreaBoundingBox.Left ||
+        _BallRect.Right >= _playAreaBoundingBox.Right)
+        {
+            _BallDirection.X*=-1;
+        }
+        // If Ball Touches the Top or Bottom
+        if(_BallRect.Bottom >= _playAreaBoundingBox.Bottom ||
+        _BallRect.Top <= _playAreaBoundingBox.Top)
+        {
+            _BallDirection.Y*=-1;
+        }
+
+    }
+
+    private void paddleCollisionCheck()
+    {
+        // Checks Collision for paddles
+
+
+            //Right Paddle
+
+            // If Ball Touches the Left or Right Side of Paddle
+            if(_BallRect.Right >= _PaddleRectR.Left &&
+            _BallRect.Left <= _PaddleRectR.Right)
+            {
+                // If Ball Touches the Top or Bottom of Paddle
+                if(_BallRect.Top <= _PaddleRectR.Bottom &&
+                _BallRect.Bottom >= _PaddleRectR.Top)
+            {
+                _BallDirection.Y*=-1;
+                _BallDirection.X*=-1;
+            }
+           
+            }
+            
+           
+
+            // Left Paddle
+
+            // If Ball Touches the Left or Right Side of Paddle
+            if(_BallRect.Left <= _PaddleRectL.Right &&
+            _BallRect.Right >= _PaddleRectL.Left)
+            {
+                // If Ball Touches the Top or Bottom of Paddle
+                if(_BallRect.Top <= _PaddleRectL.Bottom &&
+                _BallRect.Bottom >= _PaddleRectL.Top)
+            {
+                _BallDirection.Y*=-1;
+                _BallDirection.X*=-1;
+            }
+            
+            }
+           
     }
 
     protected override void Draw(GameTime gameTime)
@@ -141,8 +278,8 @@ public class Pong : Game
         // _spriteBatch.Draw(_BackgroundCourt, Vector2.Zero, null, Color.White, 0f, Vector2.Zero, backgroundScale, SpriteEffects.None, 0.1f);
         _spriteBatch.Draw(_BackgroundCourt, _playAreaBoundingBox, Color.SeaGreen);
 
-        _spriteBatch.Draw(_PaddleL, _PaddleLeft, null,  Color.White, 0f, new Vector2(1,9), 6, SpriteEffects.None, 0.1f);
-        _spriteBatch.Draw(_PaddleR, _PaddleRight, null,  Color.White, 0f, new Vector2(1,9), 6, SpriteEffects.None, 0.1f);
+        _spriteBatch.Draw(_Paddle, _PaddleRectL, Color.White);
+        _spriteBatch.Draw(_Paddle, _PaddleRectR, Color.White);
 
         // Draws Ball
             
